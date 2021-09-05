dbbe17d86904e5fa0a8c0027f8eb1207faf5ef1b
cen_shuai <cen_shuai1@163.com>
2021-09-05 20:33:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author: csstart
+ * @create: 2021-09-05 20:30
+ * @desc:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
